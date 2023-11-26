c41ca9aaded6a4e1a3a11201f988b9cff61bc9f4
Matheus Moura Martinho <matheusmouramartinho@yahoo.com>
2023-11-25 22:21:59 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tailwind-test';
+
+  cardsLR = [
+   {src: '/assets/images/food1.jpeg', alt: 'japonese food', foodDescription: '5 Bean Chilli Stew', madeBy: "Recipe by Mario"}
+  ]
+
+  cardsMPO = [
+    {src: '/assets/images/food2.jpeg', alt: 'food2', foodDescription: '3 bean chilly Stew', madeBy:"Matheus"}
+  ]
 }
+
