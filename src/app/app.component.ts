d3f79ff871bfe4afc60e3f5279da31399b9d50df
Matheus Moura Martinho <matheusmouramartinho@yahoo.com>
2023-11-27 17:42:43 -0300

    learn grid and others stuff in tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,10 @@ export class AppComponent {
   title = 'tailwind-test';
 
   cardsLR = [
-   {src: '/assets/images/food1.jpeg', alt: 'japonese food', foodDescription: '5 Bean Chilli Stew', madeBy: "Recipe by Mario"}
+   {src: '/assets/images/food1.jpeg', alt: 'japonese food', foodDescription: '5 Bean Chilli Stew', madeBy: "Recipe by Mario", minutes: '25 min'},
+
+   {src: '/assets/images/food2.jpeg', alt: 'japonese food', foodDescription: '4 Bean Chilli Stew', madeBy: "Recipe by Maria", minutes: '30 min'},
+   {src: '/assets/images/food3.jpeg', alt: 'japonese food', foodDescription: '3 Bean Chilli Stew', madeBy: "Recipe by Pedro", minutes: '15 min'}
   ]
 
   cardsMPO = [
